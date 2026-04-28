36f1ef83fa091591bf09c4ad7d9e84f31400bfb7
mathias <mathias@PORTATILINO>
2026-04-28 11:25:02 +0200

    Modifica config.java

--- src/main/java/utils/Config.java
+++ src/main/java/utils/Config.java
@@ -1,9 +1,6 @@
 package utils;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
@@ -12,123 +9,77 @@ import org.jdom2.input.SAXBuilder;
 
 
 /**
- * Classe di configurazione per l�applicazione
- * 
- * @author ste
- *
+ * Classe Config.java per la configurazione per l'applicazione
+ * @author Mathias Dal Zotto
  */
 public class Config {
-	
-	private  String driver;
-    private String url;
-    private String user;
-    private String psw;
-    private String dbType;
-    private final String xmlurl;
+	// Attributi
+	private String xml;
+	private String driver, url, user, password;
+	private String insertStudente;
+	private String insertDocente;
+	private String checkAmministratore;
+	private String checkStudente;
+	private String checkDocente;
 	
 	/**
-	 * Costruttore della classe
-	 * @param inputStream path del file di configurazione
-	 * @throws MalformedURLException 
+	 * Costruttore
+	 * @param path dell'xml di configurazione
 	 */
-	public Config(String xmlurl) throws MalformedURLException {
+	public Config(String xml) {
 		super();
-		this.xmlurl = xmlurl;
+		this.xml = xml;
 	}
 
-
 	/**
-	 * Leggo il file XML di configurazione contenuto nella cartella Config
-	 * 
-	 * @return l�elemento di root del file XML di configurazione
+	 * Funzione che restituisce l'Element root del file xml
+	 * @return root
 	 * @throws JDOMException
 	 * @throws IOException
 	 */
-	private  Element readConfig() throws JDOMException, IOException {
+	private Element readConfig() throws JDOMException, IOException {
 		SAXBuilder saxBuilder = new SAXBuilder();
-		Document document = null;
-
-		document = saxBuilder.build(new File(this.xmlurl));
-
-		Element root = document.getRootElement();
-
-
-
-		return root;
+		Document document = saxBuilder.build(this.xml);
+		return document.getRootElement();
 	}
 
 	/**
-	 * Carica il file di configurazione per recupoerare la connection string
-	 * 
+	 * Funzione che carica i parametri di configurazione
 	 * @throws IOException
 	 * @throws JDOMException
 	 */
-	public  void loadConfig() throws JDOMException, IOException {
+	public void loadConfig() throws JDOMException, IOException {
+		// db
+		Element root = readConfig();
+		Element connection = root.getChild("connection");
+		this.driver = connection.getChildText("driver").trim();
+		this.url = connection.getChildText("url").trim();
+		this.user = connection.getChildText("user").trim();
+		this.password = connection.getChildText("password").trim();
 		
-		Element root = readConfig();
-		dbType = root.getChildText("dbType");
-		Element mysql = root.getChild("connection").getChild(dbType);
-		driver = mysql.getChildText("driver").trim();
-		url = mysql.getChildText("url").trim();
-		user = mysql.getChildText("user").trim();
-		psw = mysql.getChildText("password").trim();
-
-
-		System.out.println("dbType: " + dbType);
-		System.out.println("driver: " + driver);
-		System.out.println("url: " + url);
-		System.out.println("user: " + user);
-		System.out.println("psw: " + psw);
+		// query
+		Element query = root.getChild("query");
+		this.insertStudente = query.getChildText("insertStudente").trim();
+		this.insertDocente = query.getChildText("insertDocente").trim();
+		this.checkAmministratore = query.getChildText("checkAmministratore").trim();
+		this.checkStudente = query.getChildText("checkStudente").trim();
+		this.checkDocente = query.getChildText("checkDocente").trim();
 	}
 
-	/**
-	 * Restituisce le query da eseguire
-	 * 
-	 * @param query nome del tag contenente la query nel file XNL di configurazione
-	 * @return la query da eseguire
-	 * @throws JDOMException
-	 * @throws IOException
-	 */
-	 public String getQuery(String query) throws JDOMException, IOException {
-		Element root = readConfig();
-		Element mysql = root.getChild("query").getChild(dbType);
-		return mysql.getChildText(query).trim();
-	}
-
-	/**
-	 * Restituisce il driver per il database
-	 * 
-	 * @return il driver appropriato x la connessione al db
-	 */
-	public  String getDriver() {
+	// Getter degli attributi
+	public String getDriver() {
 		return driver;
 	}
 
-	/**
-	 * Restituisce il pattern URL per il database
-	 * 
-	 * @return url per il database
-	 */
-	public  String getDbUrl() {
+	public String getUrl() {
 		return url;
 	}
 
-	/**
-	 * Restituisce il nome utente per il Database
-	 * 
-	 * @return the user
-	 */
-	public  String getUser() {
+	public String getUser() {
 		return user;
 	}
 
-	/**
-	 * Restituisce la password per il Database
-	 * 
-	 * @return the password
-	 */
-	public  String getPassword() {
-		return psw;
+	public String getPassword() {
+		return password;
 	}
-
 }
