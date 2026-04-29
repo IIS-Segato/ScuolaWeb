1ef98f5b43b4ede06bbf9974013a8f3764b9ca84
mathias <mathias@PORTATILINO>
2026-04-29 08:16:02 +0200

    Inizializzazione model e dao
    Inserimento della classe DAO e un esempio di classe nel model per lo
    sviluppo delle classi model

--- src/main/java/dao/DAO.java
+++ src/main/java/dao/DAO.java
@@ -1,9 +1,72 @@
 package dao;
 
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-public interface DAO {
+import org.jdom2.JDOMException;
 
-    void setConnection(Connection conn);
+import utils.Config;
 
+
+/**
+ * Classe DAO.java che gestisce la comunicazione con il db
+ * @author Mathias Dal Zotto
+ */
+public class DAO {
+	// Attributi
+	private Connection conn;
+	private Config conf;
+
+	/**
+	 * Costruttore
+	 * @param xml
+	 * @throws ClassNotFoundException
+	 * @throws JDOMException
+	 * @throws IOException
+	 * @throws SQLException
+	 */
+	public DAO(String xml) throws ClassNotFoundException, JDOMException, IOException, SQLException {
+		this.conf = new Config(xml);
+		this.conn = this.connect(xml);
+	}
+
+	/**
+	 * Funzione che crea la connessione col db
+	 * @param xml
+	 * @return conn
+	 * @throws JDOMException
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	private Connection connect(String xml) throws JDOMException, IOException, ClassNotFoundException, SQLException {
+		// carico il file di configurazione
+		this.conf.loadConfig();
+		
+		// leggo i parametri
+		String driver = this.conf.getDriver();
+		String url = this.conf.getUrl();
+		String user = this.conf.getUser();
+		String password = this.conf.getPassword();
+		
+		// installo i driver e creo la connessione col db
+		Class.forName(driver);
+		return DriverManager.getConnection(url + "&user=" + user + "&password=" + password);
+	}
+	
+	// Funzione per chiusura della connessione
+	public void closeConn() throws SQLException {
+		this.conn.close();
+	}
+
+	// Getter
+	public Connection getConn() {
+		return this.conn;
+	}
+	
+	public Config getConf() {
+		return this.conf;
+	}
 }
---
+++ src/main/java/model/Utente.java
@@ -0,0 +1,98 @@
+package model;
+
+import java.io.Serializable;
+
+
+/**
+ * Classe Utente.java modello della tabella utenti del db
+ * @author Mathias Dal Zotto
+ */
+public class Utente implements Serializable {
+	// Attributi
+    private static final long serialVersionUID = 1L;
+    private String username;
+    private String password;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String nascita;
+    private String citta;
+    
+    /**
+     * Costruttore
+     * @param username
+     * @param password
+     * @param nome
+     * @param cognome
+     * @param email
+     * @param nascita
+     * @param citta
+     */
+    public Utente(String username, String password, String nome, String cognome, String email, String nascita, String citta) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.email = email;
+		this.nascita = nascita;
+		this.citta = citta;
+	}
+
+	// Getter e setter degli attributi
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCognome() {
+		return cognome;
+	}
+	
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getNascita() {
+		return nascita;
+	}
+	
+	public void setNascita(String nascita) {
+		this.nascita = nascita;
+	}
+	
+	public String getCitta() {
+		return citta;
+	}
+	
+	public void setCitta(String citta) {
+		this.citta = citta;
+	}    
+}
