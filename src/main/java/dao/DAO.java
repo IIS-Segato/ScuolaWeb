e3601d91caf17ec86f820a167ceb11fe5207e83f
Aleggs <Aleggs@fedora>
2026-04-29 08:44:07 +0200

    inizializzazione classe DAO e adminDAO

--- src/main/java/dao/DAO.java
+++ src/main/java/dao/DAO.java
@@ -3,6 +3,9 @@ package dao;
 import java.sql.Connection;
 
 public interface DAO {
+	public String url = "jdbc:mysql://localhost:3306/scuolaweb";
+	public String username = "root";
+	public String password = "your_password";
 
     void setConnection(Connection conn);
 
