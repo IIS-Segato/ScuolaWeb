3c033ee6f1c6c7fa49efa3c475cf5286b4a90910
Lorenzo Follador <Lorenzo Follador@LAPTOP-EB40BBEN>
2026-05-04 10:37:20 +0200

    model studente

--- src/main/java/model/Amministratore.java
+++ src/main/java/model/Amministratore.java
@@ -3,7 +3,8 @@ package model;
 //Mettere tutti le variabili utili per l'Amministratore e metodi get and set
 
 public class Amministratore {
-
+   
+	
 	private int aid;
 	private String email;
 	private String password;
