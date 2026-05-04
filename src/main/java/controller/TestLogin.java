0bdf144b065a9d1ea910f56018fd70926a11277f
funetta <IP41-User@IP41>
2026-05-04 11:15:51 +0200

    completata funzione di autenticazione

---
+++ src/main/java/controller/TestLogin.java
@@ -0,0 +1,15 @@
+package controller;
+
+import dao.UserDao;
+
+public class TestLogin {
+
+	public static void main(String[] args) {
+		UserDao userDao = new UserDao();
+		
+		
+		System.out.println(userDao.autentica("2","anna_2005"));
+
+	}
+
+}
