752a977a1ccbcc2576c25a70cc214bb33232056a
Samuele De Bona <samueledb06@gmail.com>
2026-05-04 09:16:18 +0200

    Aggiornamento classe Persona

--- src/main/java/model/Persona.java
+++ src/main/java/model/Persona.java
@@ -1,9 +1,93 @@
 package model;
 
 public class Persona {
+	private int id;
 	private String nome;
 	private String cognome;
 	private String email;
 	private String cf;
 	private String nascita;
+	
+	/**
+	 * @param id
+	 * @param nome
+	 * @param cognome
+	 * @param email
+	 * @param cf
+	 * @param nascita
+	 */
+	public Persona(int id, String nome, String cognome, String email, String cf, String nascita) {
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.email = email;
+		this.cf = cf;
+		this.nascita = nascita;
+	}
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	/**
+	 * @return the cognome
+	 */
+	public String getCognome() {
+		return cognome;
+	}
+	/**
+	 * @param cognome the cognome to set
+	 */
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the cf
+	 */
+	public String getCf() {
+		return cf;
+	}
+	/**
+	 * @param cf the cf to set
+	 */
+	public void setCf(String cf) {
+		this.cf = cf;
+	}
+	/**
+	 * @return the nascita
+	 */
+	public String getNascita() {
+		return nascita;
+	}
+	/**
+	 * @param nascita the nascita to set
+	 */
+	public void setNascita(String nascita) {
+		this.nascita = nascita;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
 }
