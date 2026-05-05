20f0f00d41619a8ae9451470356f7b62ec82860b
Samuele De Bona <samueledb06@gmail.com>
2026-05-05 11:09:34 +0200

    Aggiunta classe Docente

---
+++ src/main/java/model/Docente.java
@@ -0,0 +1,38 @@
+package model;
+
+public class Docente {
+	private int id;
+	private int id_persona;
+	/**
+	 * @param id
+	 * @param id_persona
+	 */
+	public Docente(int id, int id_persona) {
+		this.id = id;
+		this.id_persona = id_persona;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the id_persona
+	 */
+	public int getId_persona() {
+		return id_persona;
+	}
+	/**
+	 * @param id_persona the id_persona to set
+	 */
+	public void setId_persona(int id_persona) {
+		this.id_persona = id_persona;
+	}
+}
