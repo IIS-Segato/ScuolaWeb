9b927d5f3515857bb32d41ba59c6929372f1c154
Samuele De Bona <samueledb06@gmail.com>
2026-05-05 11:05:02 +0200

    Aggiunta della classe Insegnamento

---
+++ src/main/java/model/Insegnamento.java
@@ -0,0 +1,68 @@
+package model;
+
+public class Insegnamento {
+	private int id;
+	private int id_docente;
+	private int id_materia;
+	private int id_classe;
+	/**
+	 * @param id
+	 * @param id_docente
+	 * @param id_materia
+	 * @param id_classe
+	 */
+	public Insegnamento(int id, int id_docente, int id_materia, int id_classe) {
+		this.id = id;
+		this.id_docente = id_docente;
+		this.id_materia = id_materia;
+		this.id_classe = id_classe;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the id_docente
+	 */
+	public int getId_docente() {
+		return id_docente;
+	}
+	/**
+	 * @param id_docente the id_docente to set
+	 */
+	public void setId_docente(int id_docente) {
+		this.id_docente = id_docente;
+	}
+	/**
+	 * @return the id_materia
+	 */
+	public int getId_materia() {
+		return id_materia;
+	}
+	/**
+	 * @param id_materia the id_materia to set
+	 */
+	public void setId_materia(int id_materia) {
+		this.id_materia = id_materia;
+	}
+	/**
+	 * @return the id_classe
+	 */
+	public int getId_classe() {
+		return id_classe;
+	}
+	/**
+	 * @param id_classe the id_classe to set
+	 */
+	public void setId_classe(int id_classe) {
+		this.id_classe = id_classe;
+	}
+}
