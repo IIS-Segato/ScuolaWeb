fd75f7f5957747934664d8d761c2e93e990ec5b6
Samuele De Bona <samueledb06@gmail.com>
2026-05-05 11:12:53 +0200

    Aggiunta classe AnnuncioClasse

---
+++ src/main/java/model/AnnuncioClasse.java
@@ -0,0 +1,53 @@
+package model;
+
+public class AnnuncioClasse {
+	private int id;
+	private int id_classe;
+	private int id_docente;
+	/**
+	 * @param id
+	 * @param id_classe
+	 * @param id_docente
+	 */
+	public AnnuncioClasse(int id, int id_classe, int id_docente) {
+		this.id = id;
+		this.id_classe = id_classe;
+		this.id_docente = id_docente;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the id_classe
+	 */
+	public int getId_classe() {
+		return id_classe;
+	}
+	/**
+	 * @param id_classe the id_classe to set
+	 */
+	public void setId_classe(int id_classe) {
+		this.id_classe = id_classe;
+	}
+	/**
+	 * @return the id_docente
+	 */
+	public int getId_docente() {
+		return id_docente;
+	}
+	/**
+	 * @param id_docente the id_docente to set
+	 */
+	public void setId_docente(int id_docente) {
+		this.id_docente = id_docente;
+	}
+}
