d43e8f7fbf4c4989a667ec1c811a25d012598589
Samuele De Bona <samueledb06@gmail.com>
2026-05-05 10:54:12 +0200

    Aggiunta della classe studente

--- src/main/java/model/Studente.java
+++ src/main/java/model/Studente.java
@@ -1,4 +1,53 @@
 package model;
 
 public class Studente {
+	private int id;
+	private int id_persona;
+	private int id_classe;
+	/**
+	 * @param id
+	 * @param id_persona
+	 * @param id_classe
+	 */
+	public Studente(int id, int id_persona, int id_classe) {
+		this.id = id;
+		this.id_persona = id_persona;
+		this.id_classe = id_classe;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the id_persona
+	 */
+	public int getId_persona() {
+		return id_persona;
+	}
+	/**
+	 * @param id_persona the id_persona to set
+	 */
+	public void setId_persona(int id_persona) {
+		this.id_persona = id_persona;
+	}
+	/**
+	 * @return the id_classe
+	 */
+	public int getId_classe() {
+		return id_classe;
+	}
+	/**
+	 * @param id_classe the id_classe to set
+	 */
+	public void setId_classe(int id_classe) {
+		this.id_classe = id_classe;
+	}
 }
