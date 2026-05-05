a657803370d106a5e83693c8ab863810fc616beb
Samuele De Bona <samueledb06@gmail.com>
2026-05-05 11:03:17 +0200

    Aggiunta classe Materia

---
+++ src/main/java/model/Materia.java
@@ -0,0 +1,38 @@
+package model;
+
+public class Materia {
+	private int id;
+	private String nome;
+	/**
+	 * @param id
+	 * @param nome
+	 */
+	public Materia(int id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
