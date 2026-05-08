28f71197516af4fe5a8a6b58fe0e5ff070650ab5
Samuele De Bona <samueledb06@gmail.com>
2026-05-08 06:54:27 +0200

    Aggiornamento personaDAO

--- src/main/java/dao/PersonaDAO.java
+++ src/main/java/dao/PersonaDAO.java
@@ -9,11 +9,17 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PersonaDAO {
-	private String url = "jdbc:mysql://localhost:3306/scuola";
+import org.jdom2.JDOMException;
+
+public class PersonaDAO extends AbstractDAO{
+	private static final String SQL_GET_ALL = "SELECT * FROM persone";
+	private static final String SQL_GET_BY_ID = "SELECT * FROM persone where id_persona=?";
+	private static final String SQL_INSERT = "INSERT INTO persone (name, cognome, email, cf, nascita) VALUES (?, ?, ?, ?, ?)";
+	private static final String SQL_UPDATE = "UPDATE persone SET name=?, cognome=?, email=?, cf=?, nascita=? WHERE id_persona=?";
+	private static final String SQL_DELETE = "DELETE FROM persone WHERE id_persona=?";
 	
-	public static void main(String[] args) {
-		
+	public PersonaDAO(String xml) throws ClassNotFoundException, JDOMException, IOException, SQLException {
+		super(xml);
 	}
 
 }
