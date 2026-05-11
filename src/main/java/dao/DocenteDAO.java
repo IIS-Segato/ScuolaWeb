8b7f5745d50532a9e03df6ce4529c66cc0b1f78b
Petru <Petru@AX16PRO>
2026-05-11 11:58:32 +0200

    Aggiunto il metodo per prendere tutti i docenti dal database e salvarli in una lista.

--- src/main/java/dao/DocenteDAO.java
+++ src/main/java/dao/DocenteDAO.java
@@ -1,5 +1,33 @@
 package dao;
 
-public class DocenteDAO {
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import org.jdom2.JDOMException;
 
+import model.Docente;
+
+public class DocenteDAO extends AbstractDAO {
+
+    private static final String SQL_GET_ALL  = "SELECT * FROM docenti ORDER BY cognome, nome";
+    private static final String SQL_GET_BY_ID= "SELECT * FROM docenti WHERE id = ?";
+    private static final String SQL_INSERT   = "INSERT INTO docenti (nome, cognome, materia) VALUES (?, ?, ?)";
+    private static final String SQL_UPDATE   = "UPDATE docenti SET nome=?, cognome=?, materia=? WHERE id=?";
+    private static final String SQL_DELETE   = "DELETE FROM docenti WHERE id=?";
+
+    public DocenteDao(String xml) throws ClassNotFoundException, JDOMException, IOException, SQLException {
+        super(xml);
+    }
+    
+    public List<Docente> getAll() throws Exception {
+        List<Docente> list = new ArrayList<>();
+        try (Connection c = getConnection();
+             PreparedStatement ps = c.prepareStatement(SQL_GET_ALL);
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) list.add(map(rs));
+        } catch (Exception e) { printException(e); }
+        return list;
+    }
+    
 }
