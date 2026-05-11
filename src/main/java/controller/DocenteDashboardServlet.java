a74c682f865f1186553f67702403fe522eff25f8
umberto <umbertosalvioni5@gmail.com>
2026-05-11 10:00:38 +0200

    aggiusto redirect

--- src/main/java/controller/DocenteDashboardServlet.java
+++ src/main/java/controller/DocenteDashboardServlet.java
@@ -12,27 +12,11 @@ import javax.servlet.http.HttpSession;
 
 import model.Docente;
 
-@WebServlet("/TestDashboard")
+@WebServlet("/DocenteDashboardServlet")
 public class DocenteDashboardServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
-		// Creiamo uno studente "finto" per il test
-		Docente doc = new Docente();
-		studenteTest.setNome("Mario");
-		studenteTest.setCognome("Rossi");
-		studenteTest.setClasse("5A In formatica");
-		// Nota: non serve settare la password per la dashboard
-
-		// Mettiamolo in sessione come farebbe la vera Servlet di Login
-		HttpSession session = request.getSession();
-		session.setAttribute("utenteLoggato", studenteTest);
-		session.setAttribute("ruolo", "STUDENTE");
-
-		// Vai alla dashboard (percorso corretto in WEB-INF)
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/view/studente_dashboard.jsp");
-		dispatcher.forward(request, response);
 	}
 }
