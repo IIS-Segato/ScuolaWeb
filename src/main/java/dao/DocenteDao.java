3268d30c4cbcf78c084d98c2b737cb083761bf1c
alexz <alexz@view-localhost>
2026-05-11 11:47:53 +0200

    DocenteDao Merge branch 'De-Col' of https://github.com/IIS-Segato/ScuolaWeb.git into De-Col

---
+++ src/main/java/dao/DocenteDao.java
@@ -0,0 +1,101 @@
+package dao;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.*;
+import org.jdom2.JDOMException;
+import model.Docente;
+
+public class DocenteDao extends AbstractDAO {
+
+    private static final String SQL_GET_ALL  = "SELECT * FROM docenti ORDER BY cognome, nome";
+    private static final String SQL_GET_BY_ID = "SELECT * FROM docenti WHERE id=?";
+    private static final String SQL_INSERT   = "INSERT INTO docenti (nome, cognome, materia) VALUES (?,?,?)";
+    private static final String SQL_UPDATE   = "UPDATE docenti SET nome=?, cognome=?, materia=? WHERE id=?";
+    private static final String SQL_DELETE   = "DELETE FROM docenti WHERE id=?";
+
+    public DocenteDao(String xml) throws ClassNotFoundException, JDOMException, IOException, SQLException {
+        super(xml);
+    }
+
+    public List<Docente> getAll() throws Exception {
+        List<Docente> list = new ArrayList<>();
+        try (Connection c = getConnection();
+             PreparedStatement ps = c.prepareStatement(SQL_GET_ALL);
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                Docente d = new Docente();
+                d.setId(rs.getInt("id"));
+                d.setNome(rs.getString("nome"));
+                d.setCognome(rs.getString("cognome"));
+                d.setMateria(rs.getString("materia"));
+                list.add(d);
+            }
+        } catch (Exception e) { printException(e); }
+        return list;
+    }
+
+    public Docente getByID(int id) throws Exception {
+        Docente d = null;
+        try {
+            this.conn = getConnection();
+            PreparedStatement ps = conn.prepareStatement(SQL_GET_BY_ID);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                d = new Docente();
+                d.setId(rs.getInt("id"));
+                d.setNome(rs.getString("nome"));
+                d.setCognome(rs.getString("cognome"));
+                d.setMateria(rs.getString("materia"));
+            }
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        } finally { closeConnection(); }
+        return d;
+    }
+
+    public boolean insert(String nome, String cognome, String materia) throws Exception {
+        boolean ok = false;
+        try {
+            this.conn = getConnection();
+            PreparedStatement ps = conn.prepareStatement(SQL_INSERT);
+            ps.setString(1, nome);
+            ps.setString(2, cognome);
+            ps.setString(3, materia);
+            ok = ps.executeUpdate() > 0;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        } finally { closeConnection(); }
+        return ok;
+    }
+
+    public boolean update(String nome, String cognome, String materia, int id) throws Exception {
+        boolean ok = false;
+        try {
+            this.conn = getConnection();
+            PreparedStatement ps = conn.prepareStatement(SQL_UPDATE);
+            ps.setString(1, nome);
+            ps.setString(2, cognome);
+            ps.setString(3, materia);
+            ps.setInt(4, id);
+            ok = ps.executeUpdate() > 0;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        } finally { closeConnection(); }
+        return ok;
+    }
+
+    public boolean delete(int id) throws Exception {
+        boolean ok = false;
+        try {
+            this.conn = getConnection();
+            PreparedStatement ps = conn.prepareStatement(SQL_DELETE);
+            ps.setInt(1, id);
+            ok = ps.executeUpdate() > 0;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        } finally { closeConnection(); }
+        return ok;
+    }
+}
