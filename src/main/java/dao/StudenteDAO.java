9af446e8e08bbebc2dea91d3f88ed74854725063
Samuele De Bona <samueledb06@gmail.com>
2026-05-12 08:40:15 +0200

    Aggiunta classe studente
    Merge branch 'debona' of https://github.com/IIS-Segato/ScuolaWeb into
    debona

---
+++ src/main/java/dao/StudenteDAO.java
@@ -0,0 +1,165 @@
+package dao;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jdom2.JDOMException;
+
+import model.Utente;
+
+public class StudenteDAO extends AbstractDAO{
+	private static final String SQL_GET_ALL = "SELECT * FROM utenti";
+	private static final String SQL_GET_BY_ID = "SELECT * FROM utenti WHERE id_utente=?";
+	private static final String SQL_GET_BY_PERSONA_ID = "SELECT * FROM utenti WHERE id_persona=?";
+	private static final String SQL_INSERT = "INSERT INTO utenti (username, password_hash, id_persona, id_ruolo) VALUES (?, ?, ?, ?)";
+	private static final String SQL_UPDATE = "UPDATE utenti SET username=?, password_hash=?, id_persona=?, id_ruolo=? WHERE id_utente=?";
+	private static final String SQL_DELETE = "DELETE FROM utenti WHERE id_utente=?";
+	
+	public StudenteDAO(String xml) throws ClassNotFoundException, JDOMException, IOException, SQLException {
+		super(xml);
+	}
+	
+	public List<Utente> getAll() throws Exception {
+		List<Utente> utenti = new ArrayList<>();
+		
+		try (Connection conn = getConnection();
+	         PreparedStatement ps = conn.prepareStatement(SQL_GET_ALL);
+	         ResultSet rs = ps.executeQuery())
+		{
+			while (rs.next()) {
+				Utente u = new Utente();
+                u.setId(rs.getInt("id_utente"));
+                u.setUsername(rs.getString("username"));
+                u.setPassword_hash(rs.getString("password_hash"));
+                u.setId_persona(rs.getInt("id_persona"));
+                u.setId_ruolo(rs.getInt("id_ruolo"));
+                utenti.add(u);
+            }
+			
+		} catch (Exception e) {
+			printException(e);
+		}
+		
+		return utenti;
+	}
+	
+	public Utente getById(int id) {
+		Utente u = new Utente();
+		
+		try (Connection conn = getConnection();
+	         PreparedStatement ps = conn.prepareStatement(SQL_GET_BY_ID))
+		{
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			
+		while (rs.next()) {
+			u.setId(rs.getInt("id_utente"));
+		    u.setUsername(rs.getString("username"));
+		    u.setPassword_hash(rs.getString("password_hash"));
+		    u.setId_persona(rs.getInt("id_persona"));
+		    u.setId_ruolo(rs.getInt("id_ruolo"));
+		}
+			
+		} catch (Exception e) {
+			printException(e);
+		}
+		
+		return u;
+	}
+	
+	public Utente getByStudentId(int id) {
+		Utente u = new Utente();
+		
+		try (Connection conn = getConnection();
+	         PreparedStatement ps = conn.prepareStatement(SQL_GET_BY_PERSONA_ID))
+		{
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			
+		while (rs.next()) {
+			u.setId(rs.getInt("id_utente"));
+		    u.setUsername(rs.getString("username"));
+		    u.setPassword_hash(rs.getString("password_hash"));
+		    u.setId_persona(rs.getInt("id_persona"));
+		    u.setId_ruolo(rs.getInt("id_ruolo"));
+		}
+			
+		} catch (Exception e) {
+			printException(e);
+		}
+		
+		return u;
+	}
+	
+	public boolean insert(String username, String password_hash, int id_persona, int id_ruolo) {
+		boolean isInserted = false;
+		
+		try (Connection conn = getConnection();
+	         PreparedStatement ps = conn.prepareStatement(SQL_INSERT))
+		{
+
+			ps.setString(1, username);
+			ps.setString(2, password_hash);
+			ps.setInt(3, id_persona);
+			ps.setInt(4, id_ruolo);
+			
+			if(ps.executeUpdate() > 0) {
+				isInserted = true;
+			}
+			
+		} catch (Exception e) {
+			printException(e);
+		}
+		
+		return isInserted;
+	}
+	
+	public boolean update(int id, String username, String password_hash, int id_persona, int id_ruolo) {
+		boolean isUpdated = false;
+		
+		try (Connection conn = getConnection();
+	         PreparedStatement ps = conn.prepareStatement(SQL_UPDATE))
+		{
+
+			ps.setInt(1, id);
+			ps.setString(2, username);
+			ps.setString(3, password_hash);
+			ps.setInt(4, id_persona);
+			ps.setInt(5, id_ruolo);
+			
+			if(ps.executeUpdate() > 0) {
+				isUpdated = true;
+			}
+			
+		} catch (Exception e) {
+			printException(e);
+		}
+		
+		return isUpdated;
+	}
+	
+	public boolean delete(int id) {
+		boolean isDeleted = false;
+		
+		try (Connection conn = getConnection();
+	         PreparedStatement ps = conn.prepareStatement(SQL_DELETE))
+		{
+
+			ps.setInt(1, id);
+			
+			if(ps.executeUpdate() > 0) {
+				isDeleted = true;
+			}
+			
+		} catch (Exception e) {
+			printException(e);
+		}
+		
+		return isDeleted;
+	}
+}
