16d988c2da54b1eb019e5c8e8335a6df452ba513
Samuele De Bona <samueledb06@gmail.com>
2026-05-15 16:55:33 +0200

    Aggiunta del dao di annunci classe

--- src/main/java/dao/AnnuncioClasseDAO.java
+++ src/main/java/dao/AnnuncioClasseDAO.java
@@ -15,6 +15,7 @@ import model.AnnuncioClasse;
 public class AnnuncioClasseDAO extends AbstractDAO{
 	private static final String SQL_GET_ALL = "SELECT * FROM annunci_classe";
 	private static final String SQL_GET_BY_ID = "SELECT * FROM annunci_classe WHERE id_annuncio=?";
+	private static final String SQL_GET_BY_ID_CLASSE = "SELECT * FROM annunci_classe WHERE id_classe=?";
 	private static final String SQL_INSERT = "INSERT INTO annunci_classe (id_classe, id_docente) VALUES (?, ?)";
 	private static final String SQL_UPDATE = "UPDATE annunci_classe SET id_classe=?, id_docente=? WHERE id_annuncio=?";
 	private static final String SQL_DELETE = "DELETE FROM annunci_classe WHERE id_annuncio=?";
@@ -24,30 +25,29 @@ public class AnnuncioClasseDAO extends AbstractDAO{
 	}
 	
 	public List<AnnuncioClasse> getAll() throws Exception {
-		List<AnnuncioClasse> a = new ArrayList<>();
+		List<AnnuncioClasse> annunci = new ArrayList<>();
 		
 		try (Connection conn = getConnection();
 	         PreparedStatement ps = conn.prepareStatement(SQL_GET_ALL);
 	         ResultSet rs = ps.executeQuery())
 		{
 			while (rs.next()) {
-				AnnuncioClasse i = new AnnuncioClasse();
+				AnnuncioClasse a = new AnnuncioClasse();
                 a.setId(rs.getInt("id_utente"));
-                i.setId_docente(rs.getInt("id_docente"));
-                i.setId_materia(rs.getInt("id_materia"));
-                i.setId_classe(rs.getInt("id_classe"));
-                insegnamenti.add(i);
+                a.setId_classe(rs.getInt("id_classe"));
+                a.setId_docente(rs.getInt("id_docente"));
+                annunci.add(a);
             }
 			
 		} catch (Exception e) {
 			printException(e);
 		}
 		
-		return insegnamenti;
+		return annunci;
 	}
 	
-	public Insegnamento getById(int id) {
-		Insegnamento i = new Insegnamento();
+	public AnnuncioClasse getById(int id) {
+		AnnuncioClasse a = new AnnuncioClasse();
 		
 		try (Connection conn = getConnection();
 	         PreparedStatement ps = conn.prepareStatement(SQL_GET_BY_ID))
@@ -56,29 +56,49 @@ public class AnnuncioClasseDAO extends AbstractDAO{
 			ResultSet rs = ps.executeQuery();
 			
 		while (rs.next()) {
-			i.setId(rs.getInt("id_utente"));
-            i.setId_docente(rs.getInt("id_docente"));
-            i.setId_materia(rs.getInt("id_materia"));
-            i.setId_classe(rs.getInt("id_classe"));
+			a.setId(rs.getInt("id_utente"));
+            a.setId_classe(rs.getInt("id_classe"));
+            a.setId_docente(rs.getInt("id_docente"));
 		}
 			
 		} catch (Exception e) {
 			printException(e);
 		}
 		
-		return i;
+		return a;
 	}
 	
-	public boolean insert(int id_docente, int id_materia, int id_classe) {
+	public AnnuncioClasse getByIdClasse(int id) {
+		AnnuncioClasse a = new AnnuncioClasse();
+		
+		try (Connection conn = getConnection();
+	         PreparedStatement ps = conn.prepareStatement(SQL_GET_BY_ID_CLASSE))
+		{
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			
+		while (rs.next()) {
+			a.setId(rs.getInt("id_utente"));
+            a.setId_classe(rs.getInt("id_classe"));
+            a.setId_docente(rs.getInt("id_docente"));
+		}
+			
+		} catch (Exception e) {
+			printException(e);
+		}
+		
+		return a;
+	}
+	
+	public boolean insert(int id_classe, int id_docente) {
 		boolean isInserted = false;
 		
 		try (Connection conn = getConnection();
 	         PreparedStatement ps = conn.prepareStatement(SQL_INSERT))
 		{
 
-			ps.setInt(1, id_docente);
-			ps.setInt(2, id_materia);
-			ps.setInt(3, id_classe);
+			ps.setInt(1, id_classe);
+			ps.setInt(2, id_docente);
 			
 			if(ps.executeUpdate() > 0) {
 				isInserted = true;
@@ -91,16 +111,15 @@ public class AnnuncioClasseDAO extends AbstractDAO{
 		return isInserted;
 	}
 	
-	public boolean update(int id_docente, int id_materia, int id_classe, int id) {
+	public boolean update(int id_classe, int id_docente, int id) {
 		boolean isUpdated = false;
 		
 		try (Connection conn = getConnection();
 	         PreparedStatement ps = conn.prepareStatement(SQL_UPDATE))
 		{
 
-			ps.setInt(1, id_docente);
-			ps.setInt(2, id_materia);
-			ps.setInt(3, id_classe);
+			ps.setInt(1, id_classe);
+			ps.setInt(2, id_docente);
 			ps.setInt(3, id);
 			
 			if(ps.executeUpdate() > 0) {
