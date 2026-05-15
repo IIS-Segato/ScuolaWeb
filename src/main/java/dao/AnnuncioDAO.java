850dcc66eaad8939eb5fdb7632477f798a8a0f47
Samuele De Bona <samueledb06@gmail.com>
2026-05-15 17:04:09 +0200

    Completamento del Dao annunci

--- src/main/java/dao/AnnuncioDAO.java
+++ src/main/java/dao/AnnuncioDAO.java
@@ -47,5 +47,118 @@ public class AnnuncioDAO extends AbstractDAO {
         return annunci;
     }
     
-    
+	public Annuncio getById(int id) {
+		Annuncio a = new Annuncio();
+		
+		try (Connection conn = getConnection();
+	         PreparedStatement ps = conn.prepareStatement(SQL_GET_BY_ID))
+		{
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			
+		while (rs.next()) {
+			a.setId(rs.getInt("id_annuncio"));
+            a.setTitolo(rs.getString("titolo"));
+            a.setContenuto(rs.getString("contenuto"));
+            a.setData_pubblicazione(rs.getInt("data_pubblicazione"));
+            a.setAutore(rs.getInt("autore"));
+		}
+			
+		} catch (Exception e) {
+			printException(e);
+		}
+		
+		return a;
+	}
+	
+	public Annuncio getByIdClasse(int autore) {
+		Annuncio a = new Annuncio();
+		
+		try (Connection conn = getConnection();
+	         PreparedStatement ps = conn.prepareStatement(SQL_GET_BY_AUTORE))
+		{
+			ps.setInt(1, autore);
+			ResultSet rs = ps.executeQuery();
+			
+		while (rs.next()) {
+			a.setId(rs.getInt("id_annuncio"));
+            a.setTitolo(rs.getString("titolo"));
+            a.setContenuto(rs.getString("contenuto"));
+            a.setData_pubblicazione(rs.getInt("data_pubblicazione"));
+            a.setAutore(rs.getInt("autore"));
+		}
+			
+		} catch (Exception e) {
+			printException(e);
+		}
+		
+		return a;
+	}
+	
+	public boolean insert(String titolo, String contenuto, String data_pubblicazione, int autore) {
+		boolean isInserted = false;
+		
+		try (Connection conn = getConnection();
+	         PreparedStatement ps = conn.prepareStatement(SQL_INSERT))
+		{
+
+			ps.setString(1, titolo);
+			ps.setString(2, contenuto);
+			ps.setString(3, data_pubblicazione);
+			ps.setInt(4, autore);
+			
+			if(ps.executeUpdate() > 0) {
+				isInserted = true;
+			}
+			
+		} catch (Exception e) {
+			printException(e);
+		}
+		
+		return isInserted;
+	}
+	
+	public boolean update(String titolo, String contenuto, String data_pubblicazione, int autore, int id) {
+		boolean isUpdated = false;
+		
+		try (Connection conn = getConnection();
+	         PreparedStatement ps = conn.prepareStatement(SQL_UPDATE))
+		{
+
+			ps.setString(1, titolo);
+			ps.setString(2, contenuto);
+			ps.setString(3, data_pubblicazione);
+			ps.setInt(4, autore);
+			ps.setInt(5, id);
+			
+			if(ps.executeUpdate() > 0) {
+				isUpdated = true;
+			}
+			
+		} catch (Exception e) {
+			printException(e);
+		}
+		
+		return isUpdated;
+	}
+	
+	public boolean delete(int id) {
+		boolean isDeleted = false;
+		
+		try (Connection conn = getConnection();
+	         PreparedStatement ps = conn.prepareStatement(SQL_DELETE))
+		{
+
+			ps.setInt(1, id);
+			
+			if(ps.executeUpdate() > 0) {
+				isDeleted = true;
+			}
+			
+		} catch (Exception e) {
+			printException(e);
+		}
+		
+		return isDeleted;
+	}
 }
