c93ca14386a30b2fa8ca4a1e190c3549189b6c51
Petru <Petru@AX16PRO>
2026-05-16 09:45:10 +0200

    Merge branch 'Dokaj' of https://github.com/IIS-Segato/ScuolaWeb.git into Dokaj

--- src/main/java/model/Docente.java
+++ src/main/java/model/Docente.java
@@ -1,16 +1,32 @@
 package model;
 
-	import java.io.Serializable;
+import java.io.Serializable;
 
-	public class Docente implements Serializable {
+public class Docente implements Serializable {
 
-	    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	    private int    id;
-	    private String nome;
-	    private String cognome;
-	    private String materia;
+    private int    id;
+    private String nome;
+    private String cognome;
+    private String materia;
 
-	
+    public Docente() {}
 
+    public Docente(int id, String nome, String cognome, String materia) {
+        this.id = id;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.materia = materia;
+    }
+
+    public int    getId(){ return id; }
+    public String getNome(){ return nome; }
+    public String getCognome(){ return cognome; }
+    public String getMateria(){ return materia; }
+
+    public void setId(int id){ this.id      = id; }
+    public void setNome(String nome){ this.nome    = nome; }
+    public void setCognome(String c){ this.cognome = c; }
+    public void setMateria(String m){ this.materia = m; }
 }
