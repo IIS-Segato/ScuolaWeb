49895b687428e2f42af147c69011cc0f90951c45
Samuele De Bona <samueledb06@gmail.com>
2026-05-20 12:14:04 +0200

    Miglioramento della classe OrarioDAO e funzione doPost di OrarioController

--- src/main/java/controller/OrarioController.java
+++ src/main/java/controller/OrarioController.java
@@ -12,10 +12,11 @@ import jakarta.servlet.http.HttpServletResponse;
 public class OrarioController extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	
+	private static final String ACTION_GET_ALL = "GETALL";
+	private static final String ACTION_GET_BY_ID = "GETBYID";
 	private static final String ACTION_INSERT = "INSERT";
 	private static final String ACTION_DELETE = "DELETE";
 	private static final String ACTION_UPDATE = "UPDATE";
-	private static final String ACTION_EDIT = "EDIT";
 	
 	private OrarioDAO orarioDao = null;
        
@@ -54,7 +55,39 @@ public class OrarioController extends HttpServlet{
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		try {
+			String id = request.getParameter("id");
+			String id_insegnamento = request.getParameter("id_insegnamento");
+			String id_aula = request.getParameter("id_aula");
+			String giorno = request.getParameter("giorno");
+			String ora_inizio = request.getParameter("ora_inizio");
+			String ora_fine = request.getParameter("ora_fine");
+			String action = request.getParameter("action");
+			
+			if(ACTION_INSERT.equals(action)) {
+				orarioDao.insert(Integer.parseInt(id_insegnamento),
+						Integer.parseInt(id_aula),
+						giorno,
+						ora_inizio,
+						ora_fine
+						);
+			}
+			else if(ACTION_UPDATE.equals(action)){
+				orarioDao.update(Integer.parseInt(id_insegnamento),
+						Integer.parseInt(id_aula),
+						giorno,
+						ora_inizio,
+						ora_fine,
+						Integer.parseInt(id)
+						);
+			}
+			
+			response.sendRedirect("Voto");
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			throw new ServletException(e.getMessage());
+		}
 	}
 
 }
