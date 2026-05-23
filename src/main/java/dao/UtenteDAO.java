6634eb98b8706797906742eb8b5fd0646b80d902
simone <simone@DESKTOP-E052R7P>
2026-05-23 09:20:07 +0200

    UtenteDAO sql

--- src/main/java/dao/UtenteDAO.java
+++ src/main/java/dao/UtenteDAO.java
@@ -1,5 +1,21 @@
 package dao;
 
-public class UtenteDAO {
+import java.io.IOException;
+import java.sql.SQLException;
 
+import org.jdom2.JDOMException;
+
+public class UtenteDAO extends AbstractDAO {
+
+	private static final String SQL_LOGIN = "SELECT * FROM utenti WHERE username = ? AND password = ?";
+	private static final String SQL_GET_ALL = "SELECT * FROM utenti ORDER BY cognome, nome";
+	private static final String SQL_GET_BY_ID = "SELECT * FROM utenti WHERE id = ?";
+	private static final String SQL_INSERT = "INSERT INTO utenti (username, password, ruolo, nome, cognome) VALUES (?, ?, ?, ?, ?)";
+	private static final String SQL_UPDATE = "UPDATE utenti SET username=?, ruolo=?, nome=?, cognome=? WHERE id=?";
+	private static final String SQL_UPDATE_PWD = "UPDATE utenti SET password=? WHERE id=?";
+	private static final String SQL_DELETE = "DELETE FROM utenti WHERE id=?";
+
+	public UtenteDAO(String xml) throws ClassNotFoundException, JDOMException, IOException, SQLException {
+		super(xml);
+	}
 }
