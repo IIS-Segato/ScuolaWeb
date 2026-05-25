b0c3ada9b7957f8fcb847fbb8c855e43ffc1c7bc
umberto <umbertosalvioni5@gmail.com>
2026-05-25 11:13:59 +0200

    aggiustato voto dao

--- src/main/java/dao/VotoDao.java
+++ src/main/java/dao/VotoDao.java
@@ -1,10 +1,14 @@
 package dao;
 
 import java.io.IOException;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import org.jdom2.JDOMException;
+import model.Voto;
 
 public class VotoDao extends DAO {
 
@@ -12,6 +16,31 @@ public class VotoDao extends DAO {
         super(xmlurl);
     }
 
+    public List<Voto> getVotiByStudente(int idStudente) {
+        List<Voto> listaVoti = new ArrayList<>();
+        String query = config.getQuery("voti", "selectByStudente");
+
+        try (PreparedStatement ps = this.conn.prepareStatement(query)) {
+            ps.setInt(1, idStudente);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    Voto v = new Voto();
+                    v.setId(rs.getInt("id"));
+                    v.setVoto(rs.getDouble("voto"));
+                    v.setData(rs.getDate("data"));
+                    v.setDescrizione(rs.getString("descrizione"));
+                    v.setNomeDocente(rs.getString("nome_docente") + " " + rs.getString("cognome_docente"));
+                    v.setNomeMateria(rs.getString("nome_materia"));
+                    listaVoti.add(v);
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Errore in VotoDao: " + e.getMessage());
+        }
+        return listaVoti;
+    }
+    
     public boolean insertVoto(int idStudente, int idDocente, double voto, String dataStr, String descrizione) {
         // Recupera la query definita nel tuo dbcfg.xml under <voti><insert>
         String query = config.getQuery("voti", "insert");
@@ -31,3 +60,6 @@ public class VotoDao extends DAO {
         }
     }
 }
+
+
+
